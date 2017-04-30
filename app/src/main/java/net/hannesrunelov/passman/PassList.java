423636518918f89dccb4d3a2fddf6601037581c4
Hannes Runelöv <hrunelov@kth.se>
2017-04-30 11:56:04 +0200

    Removed derpy test code

--- app/src/main/java/net/hannesrunelov/passman/PassList.java
+++ app/src/main/java/net/hannesrunelov/passman/PassList.java
@@ -10,14 +10,5 @@ public class PassList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_list);
-
-        TextView text = (TextView)this.findViewById(R.id.text);
-        StringBuilder sb = new StringBuilder();
-        sb.append(PasswordGenerator.getPassword("", "", 32)+"\n");
-        sb.append(PasswordGenerator.getPassword("a", "b", 32)+"\n");
-        sb.append(PasswordGenerator.getPassword("a", "b", 32)+"\n");
-        sb.append(PasswordGenerator.getPassword("aa", "bb", 32)+"\n");
-        sb.append(PasswordGenerator.getPassword("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC", "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC", 32)+"\n");
-        text.setText(sb.toString());
     }
 }
